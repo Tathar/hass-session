3104738d582c610564585eead9848ea959d45637
Tathar <tathar.dev@gmail.com>
2022-02-07 19:11:59 +0100

    update

--- ha_session.py
+++ ha_session.py
@@ -2,85 +2,22 @@ import win32service
 import win32serviceutil
 import win32api
 import win32event
-import subprocess
 import sys
 import socket
 import servicemanager
 
-import requests
+from hass_api import *
+from win32_session import *
+
 from os import path
 from configobj import ConfigObj
-
-import win32ts
-
-from win32com.shell import shell
-
-protocols = {
-    win32ts.WTS_PROTOCOL_TYPE_CONSOLE: "console",
-    win32ts.WTS_PROTOCOL_TYPE_ICA: "citrix",
-    win32ts.WTS_PROTOCOL_TYPE_RDP: "rdp",
-}
-
-
-def close(id):
-    """fermer la seesion"""
-    # ctypes.windll.user32.ExitWindowsEx(0, 1)
-    # print("logoff {}".format(id))
-    servicemanager.LogInfoMsg("logoff {}".format(id))
-    subprocess.call(["logoff", str(id)])
-
-
-def connected_user():
-    # out = subprocess.check_output("quser", text=True, encoding="utf16")
-    hServer = win32ts.WTS_CURRENT_SERVER_HANDLE
-    for session in win32ts.WTSEnumerateSessions(hServer):
-        sessionId = session["SessionId"]
-        session["UserName"] = win32ts.WTSQuerySessionInformation(
-            hServer, sessionId, win32ts.WTSUserName)
-        session[
-            "WinStationName"] = session["WinStationName"] or "(disconnected)"
-        session["Protocol"] = win32ts.WTSQuerySessionInformation(
-            hServer, sessionId, win32ts.WTSClientProtocolType)
-        session["ProtocolName"] = protocols.get(session["Protocol"], "unknown")
-
-        # print(session)
-        yield session
-
-
-class HASS_API:
-    def __init__(self, ip, port, key, url="/api", https=False):
-
-        if https:
-            self.api = "https://"
-        else:
-            self.api = "http://"
-
-        self.api = self.api + str(ip) + ":" + str(port) + str(url)
-
-        self.header = {
-            "Authorization": "Bearer {}".format(key),
-            "content-type": "application/json",
-        }
-
-    def write(self, id: str):
-        url = self.api + "/services/input_boolean/turn_on"
-        data = {"entity_id": id}
-        res = requests.post(url, json=data, headers=self.header)
-        res.raise_for_status()
-
-    def read(self, id: str) -> int:
-        url = self.api + "/states/" + id
-        res = requests.get(url, timeout=11, headers=self.header)
-        res.raise_for_status()
-        return int(float(res.json()["state"]))
 
 
 class Session:
     def __init__(self):
         self.run = True
         # self.connected = False
-        self.config = ConfigObj(
-            path.expandvars(r"%ProgramData%\ha_session\config.ini"))
+        self.config = ConfigObj(path.expandvars(r"%ProgramData%\ha_session\config.ini"))
         self.ha = HASS_API(**self.config["HASS"])
         self.error = 0
         self.old_user = dict()
@@ -90,16 +27,11 @@ class Session:
         for session in connected_user():
             user = session["UserName"]
             user_id = session["SessionId"]
-            if (session["WinStationName"] !=
-                    "Console"):  # si l'utilisateur est un service
-                continue
 
-            print(session)
             if user in self.config["USERS"].keys():
 
                 if user_id not in self.old_user.keys():
-                    self.old_user[
-                        user_id] = user  # on associe le user avec le userid
+                    self.old_user[user_id] = user  # on associe le user avec le userid
                     servicemanager.LogInfoMsg("login of {}".format(user))
 
                 self.action(user, user_id)
@@ -107,8 +39,10 @@ class Session:
                 self.config["USERS"].rename([self.old_user[user_id]], user)
                 self.config.write()
                 self.action(user, user_id)
-            elif (user not in self.config["USERS"].keys() and user_id
-                  in self.old_user.keys()):  # l'utilisateur c'est déconnecté
+            elif (
+                user not in self.config["USERS"].keys()
+                and user_id in self.old_user.keys()
+            ):  # l'utilisateur c'est déconnecté
                 servicemanager.LogInfoMsg("logout of {}".format(user))
                 del self.old_user[user_id]
 
@@ -116,26 +50,23 @@ class Session:
         ret = 0
         # self.connected = True
         # while self.run and self.connected and is_running:
+        ha_session = HASS_SESSION(api=self.ha, **self.config["USERS"][user])
         try:
-            ret = self.ha.read(self.config["USERS"][user]["counter"])
-            print(ret)
+            if ha_session.end():
+                self.error = 0
+                servicemanager.LogInfoMsg("forced logout of {}".format(user))
+                close(user_id)
         except:
             self.error += 1
-            servicemanager.LogInfoMsg("connection error n = {}".format(
-                self.error))
-        else:
-            self.error = 0
-            if ret == 0:
-                # self.connected = False
-                close(user_id)
+            servicemanager.LogInfoMsg("connection error n = {}".format(self.error))
 
-        if self.error > 10:
+        if self.error > 60:
             self.error = 0
             # self.connected = False
-            servicemanager.LogInfoMsg("forced logout of {}".format(user))
+            servicemanager.LogInfoMsg(
+                "connection error forced logout of {}".format(user)
+            )
             close(user_id)
-
-        self.ha.write(self.config["USERS"][user]["session"])
 
     def stop(self):
         self.run = False
@@ -172,8 +103,7 @@ class Service(win32serviceutil.ServiceFramework):
             self.ReportServiceStatus(win32service.SERVICE_RUNNING)
             self.log("Service is starting.")
             self.main()
-            win32event.WaitForSingleObject(self.stop_event,
-                                           win32event.INFINITE)
+            win32event.WaitForSingleObject(self.stop_event, win32event.INFINITE)
             servicemanager.LogMsg(
                 servicemanager.EVENTLOG_INFORMATION_TYPE,
                 servicemanager.PYS_SERVICE_STARTED,
---
+++ hass_api.py
@@ -0,0 +1,62 @@
+from time import sleep
+import requests
+
+
+class HASS_API:
+    def __init__(self, ip, port, key, url="/api", https=False):
+
+        if https:
+            self.api = "https://"
+        else:
+            self.api = "http://"
+
+        self.api = self.api + str(ip) + ":" + str(port) + str(url)
+
+        self.header = {
+            "Authorization": "Bearer {}".format(key),
+            "content-type": "application/json",
+        }
+
+    def write(self, id: str):
+        url = self.api + "/services/input_boolean/turn_on"
+        data = {"entity_id": id}
+        res = requests.post(url, json=data, headers=self.header)
+        res.raise_for_status()
+
+    def read(self, id: str) -> int:
+        url = self.api + "/states/" + id
+        res = requests.get(url, timeout=11, headers=self.header)
+        res.raise_for_status()
+        return int(float(res.json()["state"]))
+
+
+class HASS_SESSION:
+    def __init__(self, api: HASS_API, counter, session) -> None:
+        self.api: HASS_API = api
+        self._counter = counter
+        self._session = session
+
+    def end(self) -> bool:
+        self.api.write(self._session)
+        print("counter = {}".format(self.api.read(self._counter)))
+        return not self.api.read(self._counter) > 0
+
+    @property
+    def counter(self) -> int:
+        return self.api.read(self._counter)
+
+
+if __name__ == "__main__":
+    from os import path
+    from configobj import ConfigObj
+
+    config = ConfigObj(path.expandvars(r"config.ini"))
+    ha = HASS_API(**config["HASS"])
+
+    ha_session = HASS_SESSION(ha, **config["USERS"]["test"])
+    while not ha_session.end():
+        sleep(10)
+        print("connected {}".format(ha_session.counter))
+
+    if ha_session.end():
+        print("logout")
